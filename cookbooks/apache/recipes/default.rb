d7c22f7615f839e30f2b2a0d75cdb74b3141d86a
Adrian <stewy.aj@gmail.com>
2017-09-24 09:35:07 +0000

    added motd and changed apache default

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,12 +5,20 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
 # Install apache package
+
+if node['platform_family'] == "rhel"   		# evaluates the node to see if it is rhel
+	package = "httpd"			# sets the variable package to httpd
+elseif node node['platform_family'] == "debian"	# evaluates the node to see if it is debian
+	package = "apache2"			# sets the variable package to apache2
+end
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package			# sets the package_name installs either httpd or apache2 depending on family name 
 	action :install
 end
 
 service 'apache2' do
 	service_name 'httpd'
-	action[:start, :enable]
+	action [:start, :enable]
 end
+
